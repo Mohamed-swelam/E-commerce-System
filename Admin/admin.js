704bd7f2fbbe93aa2a07fae862a22a1b1b4f553e
nawal-ali <nmaaa3899@gmail.com>
2026-02-19 00:59:06 +0200

    fixed some layout bugs

--- Admin/admin.js
+++ Admin/admin.js
@@ -3,10 +3,9 @@ subTabs = document.querySelectorAll('.sub-tab')
 
 mainTabs.forEach(p => {
     p.addEventListener('click', (e) => {
-        mainTabs.forEach(t => t.classList.remove('active-tab'))
+        mainTabs.forEach(p => p.classList.remove('active-tab'))
         p.classList.add('active-tab')
         subTabs.forEach(div => { div.classList.remove('active-sub-tab') })
-        // console.log(e.target.innerHTML);
         subTabs.forEach(div => {
             if (div.id == e.target.innerHTML) {
                 div.classList.add('active-sub-tab')
