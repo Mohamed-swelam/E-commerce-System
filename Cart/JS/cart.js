2f4aac2b5b408a94fd5b57196bd1d1315ddb3168
Mohamed Swelam <swelamm015@gmail.com>
2026-02-25 21:52:28 +0200

    Implement Navbar and Footer Component and fetch them

--- Cart/JS/cart.js
+++ Cart/JS/cart.js
@@ -265,29 +265,4 @@ document.getElementById("go_Checkout").addEventListener("click", function () {
     window.location.href = "../../Checkout/Checkout.html";
 })
 
-displaycart();
-
-
-
-
-function showToast(message, type = "success") {
-    const toastEl = document.getElementById("mainToast");
-    const toastMsg = document.getElementById("toastMessage");
-
-
-    if (type === "success") {
-        toastEl.className = "toast align-items-center text-white bg-success border-0";
-    } else if (type === "error") {
-        toastEl.className = "toast align-items-center text-white bg-danger border-0";
-    } else if (type === "warning") {
-        toastEl.className = "toast align-items-center text-dark bg-warning border-0";
-    }
-
-    toastMsg.textContent = message;
-
-    const toast = new bootstrap.Toast(toastEl, {
-        delay: 3000,
-    });
-
-    toast.show();
-}
+displaycart()
