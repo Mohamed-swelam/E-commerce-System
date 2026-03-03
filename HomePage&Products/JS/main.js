0f447c0c5f1f45e94fdd17b812db17615b463533
Mohamed Swelam <swelamm015@gmail.com>
2026-03-03 16:04:52 +0200

    showing the profile info to customer and admin and adding modal in customer info

--- HomePage&Products/JS/main.js
+++ HomePage&Products/JS/main.js
@@ -415,7 +415,7 @@ if (currentUser) {
 
   if (currentUser.role === "admin" || currentUser.role === "seller") {
 
-    userProfile && (userProfile.style.display = "none");
+    userProfile && (userProfile.style.display = "block");
     cartIcon && (cartIcon.style.display = "none");
     wishlistIcon && (wishlistIcon.style.display = "none");
 
